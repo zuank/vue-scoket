1be95f236da43db3ecfc425ba92dcf34801db570
Yuehao Wang <352209095@qq.com>
2017-03-28 16:22:42 +0800

    init

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const httpProxy = require('http-proxy');
 const configIO = require('./io');
 
 const app = express();
-const path = require('path');
+// const path = require('path');
 
 const port = process.env.PORT || 3000;
 
@@ -18,6 +18,6 @@ const server = app.listen(port, () => {
 httpProxy.createProxyServer({
   target: 'http://172.16.40.8:8080',
 }).listen(3030);
-const io = require('socket.io').listen(server); 
+const io = require('socket.io').listen(server);
 // 接入io配置
 configIO(io);
