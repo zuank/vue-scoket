cfa660828786d85ce10398b8f7de0d0df0b460a2
Yuehao Wang <352209095@qq.com>
2017-03-29 14:09:50 +0800

    add page

---
+++ src/page/Login.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="login">
+    <h3>登录</h3>
+    <div class="form">
+     <p><span>账户名：</span><input type="text"></p>
+     <p><span>密码</span><input type="password"></p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'login',
+  };
+
+</script>
+
+<style scoped>
+.login{
+  width: 80%;
+  margin: 30px auto;
+  box-shadow: 0 0 1px #ddd;
+  text-align: center;
+  border-radius: 5px;
+}
+
+</style>
